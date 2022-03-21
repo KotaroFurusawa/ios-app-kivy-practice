340f726ac0d969f87e4b5a4b16a18a50b37f9ea3
KotaroFurusawa <kotaro.furusawa@clipline.jp>
2022-03-21 21:18:34 +0900

    HomeScreenのデザイン(ボタン押下時アニメーション)

--- main.py
+++ main.py
@@ -1,9 +1,15 @@
 from kivy.app import App
 from kivy.lang import Builder
 from kivy.uix.screenmanager import Screen
+from kivy.uix.button import ButtonBehavior
+from kivy.uix.image import Image
 
 
 class HomeScreen(Screen):
+    pass
+
+
+class ImageButton(ButtonBehavior, Image):
     pass
 
 
