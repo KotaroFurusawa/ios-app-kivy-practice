e734a6c10ba3d592ffa4b51eb0b487540340d593
KotaroFurusawa <kotaro.furusawa@clipline.jp>
2022-03-25 22:16:02 +0900

    workoutの一覧を表示する機能の追加

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from kivy.lang import Builder
 from kivy.uix.screenmanager import Screen
 from kivy.uix.button import ButtonBehavior
 from kivy.uix.image import Image
+from workoutbanner import WorkoutBanner
 import requests
 import json
 
@@ -31,7 +32,6 @@ class MainApp(App):
 
     def on_start(self):
         # dbからデータ取得
-        # 人気のトレーニングをホームスクリーンに表示
         api_url = self.secret['FIREBASE']['R_DB']['URL']
         result = requests.get(f"{api_url}{self.my_friend_id}.json")
         data = json.loads(result.content.decode())
@@ -44,10 +44,20 @@ class MainApp(App):
         avatar_image = self.root.ids["home_screen"].ids["avatar_image"]
         avatar_image.source = "icons/avatars/" + data["avatar"]
 
+        # 人気のトレーニングをホームスクリーンに表示
+        banner_grid = self.root.ids["home_screen"].ids["banner_grid"]
         workouts = data["workouts"][1:]
         for workout in workouts:
             print(workout["workout_img"])
-            print(workout["units"])
+            print(workout["description"])
+            w = WorkoutBanner(
+                workout_img=workout["workout_img"],
+                description=workout["description"],
+                type_image=workout["type_image"],
+                number=workout["number"],
+                units=workout["units"],
+                likes=workout["likes"])
+            banner_grid.add_widget(w)
 
     def change_screen(self, screen_name):
         screen_manager = self.root.ids["screen_manager"]
